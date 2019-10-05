ca2fec37934fdf1bb109261a5397434ff4942cf7
stteffano13 <tefo.aguayo@gmail.com>
2019-10-04 23:17:07 -0500

    inicio proyecto valid
    sadsd

--- ejemplo/APPMOVILCHOFER/appChofer2/src/app/services/global.ts
+++
@@ -1,5 +0,0 @@
-export var GLOBAL = {
- url: "http://www.appmontecarlotransvip.com:3977/api/"
-   //url: "http://localhost:3977/api/"
-    //url:"http://192.168.1.8:3977/api/"
-};
--- ejemplo/APPMOVILCLIENTE/src/app/services/global.ts
+++
@@ -1,5 +0,0 @@
-export var GLOBAL = {
- //url: "http://www.appmontecarlotransvip.com:3977/api/"
- url:"http://localhost:3977/api/"
- //url:"http://10.0.0.7:3977/api/"
-};
