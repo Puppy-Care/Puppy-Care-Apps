9780a7e4fe33deda0791ec012aba99292d73568d
stteffano13 <tefo.aguayo@gmail.com>
2019-10-08 18:06:11 -0500

    inicio servicio

---
+++ api/appi.js
@@ -0,0 +1,49 @@
+'Use strict'
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+//Notificaciones
+
+
+
+
+//cargar Rutas
+/*var administrador_rutes = require('./routes/administrador');
+var docente_rutes = require('./routes/docente');
+var estudiante_rutes = require('./routes/estudiante');
+var curso_rutes = require('./routes/curso');
+var matricula_rutes = require('./routes/matricula');
+var materia_rutes = require('./routes/materia');
+var nota_rutes = require('./routes/nota');
+var insumo_rutes = require('./routes/insumo');*/
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json()); //convertir a json als peticiones
+
+//configurar cabeceras http
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+ 
+    next();
+});
+
+// rutas base
+/*app.use('/api', administrador_rutes);
+app.use('/api', docente_rutes);
+app.use('/api', estudiante_rutes);
+app.use('/api', curso_rutes);
+app.use('/api', matricula_rutes);
+app.use('/api', materia_rutes);
+app.use('/api', nota_rutes);
+app.use('/api', insumo_rutes);*/
+
+
+
+
+
+module.exports = app; // hace referencia a la variable de express
+
