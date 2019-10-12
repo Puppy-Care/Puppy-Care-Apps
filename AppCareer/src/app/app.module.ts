bdad6faf475f6e8c258ec07590d5cd05eba1f2a2
vanessitaf8 <caryto_z@hotmail.com>
2019-10-12 18:52:16 -0500

    ultima

--- AppCareer/src/app/app.module.ts
+++ AppCareer/src/app/app.module.ts
@@ -24,7 +24,7 @@ import { CallNumber } from '@ionic-native/call-number';
     HttpModule,
     IonicModule.forRoot(MyApp),
     AgmCoreModule.forRoot({
-      apiKey: 'AIzaSyBpvyrZXYTUxsVOnNQQS9zNCxN9Ti9azP0'
+      apiKey: 'AIzaSyARuGaeV-rD_M_ZP2uZT6d8SzFmywzAbRk'
     })
   ],
   bootstrap: [IonicApp],
